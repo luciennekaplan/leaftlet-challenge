51723e4f8973a117575c46ff7c956a014d806fda
luciennekaplan <lucykaplan@Lucys-MacBook-Air.local>
2021-02-03 14:20:14 -0800

    Final Draft

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,11 @@
 // API
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-// 
 var myMap = L.map("mapid", {
     center: [40.7608, -111.8910],
     zoom: 5,
     });
+
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -14,7 +14,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     id: "mapbox/light-v10",
     accessToken: "pk.eyJ1IjoibHVjaWVubmVrYXBsYW4iLCJhIjoiY2trOHowdGN3MHNyODJ5bnRmbmg5b2t0dyJ9.46ciJtDd5-UKUy6rTirCgA",
-    }).addTo(myMap);
+}).addTo(myMap);
 
 function findRadius(magnitude) {
     return magnitude * 4;
@@ -42,6 +42,7 @@ d3.json(url).then(function(data) {
             fillOpacity: 0.5
           });
         },
+        
         onEachFeature: function(feature, layer) {
             // Set mouse events to change map styling
             layer.on({
@@ -61,7 +62,7 @@ d3.json(url).then(function(data) {
               },
               // Show pop-up when feature is clicked on
               click: function(event) {
-                layer.bindPopup("<h3>" + `Location: ${feature.properties.place}`+ "</h3><h3>" + `Magnitude: ${feature.properties.mag}` + "</h3>");
+                layer.bindPopup("<h3>" + `Recorded location: ${feature.properties.place}`+ "</h3><h3>" + `Magnitude: ${feature.properties.mag}` + "</h3>");
               }
             });
           }
