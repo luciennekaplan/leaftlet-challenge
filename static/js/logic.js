b355a05f1ecb7f7f9d1dd2b3f441c6458aa9a0f7
luciennekaplan <lucykaplan@Lucys-MacBook-Air.local>
2021-02-02 21:24:21 -0800

    added circle markers

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,24 +1,27 @@
-// API
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Creating map object
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
 var myMap = L.map("mapid", {
-  center: [45.5051, -122.6750],
-  zoom: 11
-});
+    center: [14.4974, 14.4524],
+    zoom: 2,
+    });
 
-// Adding tile layer to the map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: "pk.eyJ1IjoibHVjaWVubmVrYXBsYW4iLCJhIjoiY2trOHowdGN3MHNyODJ5bnRmbmg5b2t0dyJ9.46ciJtDd5-UKUy6rTirCgA",
-}).addTo(myMap);
-
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: "pk.eyJ1IjoibHVjaWVubmVrYXBsYW4iLCJhIjoiY2trOHowdGN3MHNyODJ5bnRmbmg5b2t0dyJ9.46ciJtDd5-UKUy6rTirCgA",
+    }).addTo(myMap);
 
 d3.json(url).then(function(data) {
-
-    console.log(data);
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+          return new L.CircleMarker(latlng, {
+            radius: 5,
+            color: 'red'
+          });
+        },
+      }).addTo(myMap);
 });
